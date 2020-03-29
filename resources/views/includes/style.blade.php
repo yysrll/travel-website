f6bf6ffabd6131e7c7a40120f978027ad2b990b0
yysrll <yusril.uzumaki@gmail.com>
2020-03-30 00:55:05 +0800

    merapikan layout admin

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,7 @@
+
+  <!-- Custom fonts fo this template-->
+  <link href="{{ url('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles fo this template-->
+  <link href="{{ url('backend/css/sb-admin-2.min.css')}}" rel="stylesheet">
