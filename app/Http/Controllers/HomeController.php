381052958bf627c4c3bf4607f64a53c416aa2782
yysrll <yusril.uzumaki@gmail.com>
2020-04-05 20:51:20 +0800

    update laravel 7, mengatur autentikasi admin dan verifikasi email

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,15 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(Request $request) {
+
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
         return view('pages.home');
     }
 }
