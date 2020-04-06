6b3a794003f371c0611787fd9fca73e537016156
yysrll <yusril.uzumaki@gmail.com>
2020-04-06 23:04:51 +0800

    travel-package index

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,8 @@ Route::prefix('admin')
     ->group(function() {
         Route::get('/', 'DashboardController@index')
             ->name('dashboard');
+
+        Route::resource('travel-package', 'TravelPackageController');
     });
 
 Auth::routes(['verify' =>true]);
