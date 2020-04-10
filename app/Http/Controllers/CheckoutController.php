3d5c682755ef89b1696db4e1af76023f2ef82ad3
yysrll <yusril.uzumaki@gmail.com>
2020-04-10 22:28:16 +0800

    integrasi halaman checkout tahap pertama

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,15 +2,88 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Transaction;
+use App\TransactionDetail;
+use App\TravelPackage;
+
+use Carbon\Carbon;
+
+use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
 {
-    public function index(Request $request) {
-        return view('pages.checkout');
+    public function index(Request $request, $id) {
+        $item = Transaction::with(['details', 'travel_package', 'user'])->findOrFail($id);
+
+        return view('pages.checkout', [
+            'item' => $item
+        ]);
+    }
+    
+    public function process(Request $request, $id) {
+        $travel_package = TravelPackage::findOrFail($id);
+
+        $transaction = Transaction::create([
+            'travel_packages_id' => $id,
+            'users_id' => Auth::user()->id,
+            'additional_visa' => 0,
+            'transaction_total' => $travel_package->price,
+            'transaction_status' => 'IN_CART'
+        ]);
+
+        return redirect()->route('checkout', $transaction->id);
+    }
+    
+    public function remove(Request $request, $detail_id) {
+        $item = TransactionDetail::findOrFail($detail_id);
+
+        $transaction = Transaction::with(['details', 'travel_package'])->findOrFail($item->transactions_id);
+
+        if($item->is_visa) {
+            $transaction->transaction_total -= 190;
+            $transaction->additional_visa -= 190;
+        }
+
+        $transaction->transaction_total -= $transaction->travel_package->price;
+
+        $transaction->save();
+        $item->delete();
+
+        return redirect()->route('checkout', $item->transaction_id);
+    }
+    
+    public function create(Request $request, $id) {
+        $request->validate([
+            'username' => 'required|string|exists:user,username',
+            'is_visa' => 'required|boolean',
+            'doe_passport' => 'required'
+        ]);
+
+        $data = $request->all();
+        $data['transactions_id'] = $id;
+
+        TransactionDetail::create($data);
+
+        $transaction = Transaction::with(['travel_package'])->find($id);
+
+        if($request->is_visa) {
+            $transaction->transaction_total += 190;
+            $transaction->additional_visa += 190;
+        }
+
+        $transaction->transaction_total += $transaction->travel_package->price;
+
+        $transaction->save();
+
+        return redirect()->route('checkout', $id);
     }
 
-    public function success(Request $request) {
+    public function success(Request $request, $id) {
+        $transaction = Transaction::findOrFail($id);
+        $transaction->transaction_status = 'PENDING';
+
+        $transaction->save();
+
         return view('pages.success');
     }
 }
