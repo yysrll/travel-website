ca66b9b8f55b0cc05b01701d9ac6880d3d5e041d
yysrll <yusril.uzumaki@gmail.com>
2020-04-10 18:19:47 +0800

    integrasi halaman utama

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\TravelPackage;
+
 class HomeController extends Controller
 {
 
@@ -15,6 +17,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.home');
+        $items = TravelPackage::with(['galleries'])->get();
+        return view('pages.home', [
+            'items' => $items
+        ]);
     }
 }
